f90864fec0802ec3129b0910bd3feb4b28748680
Stephanie Benita Hannemann-Pena <shannema@student.42heilbronn.de>
2025-11-03 21:53:47 +0100

    Added memmove, to be corrected and studied

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,39 +6,39 @@
 /*   By: shannema <shannema@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 18:13:36 by shannema          #+#    #+#             */
-/*   Updated: 2025/11/03 20:13:25 by shannema         ###   ########.fr       */
+/*   Updated: 2025/11/03 21:53:11 by shannema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char		*ptr_dest;
 	const char	*ptr_src;
-	// 	 The  memmove() function copies n bytes from memory area src to memory area dest.
+	size_t		i;
 
-	ptr_dest = (char *) dest;
-	ptr_src = (char *) src;
-	if (dest == NULL && src == NULL)
+	if (dest == NULL || src == NULL)
 		return (NULL);
-	while (n > 0)
-	{	
-		*ptr_dest++ = *ptr_src++;
-		n--;
-	if (dest[n] == src[n])
+	ptr_dest = (char *) dest;
+	ptr_src = (const char *) src;
+	i = 0;
+	if (ptr_dest < ptr_src)
+	{
+		while (i < n)
 		{
-		src[n] = ptr_dest;
+			ptr_dest[i] = ptr_src[i];
+			i++;
 		}
 	}
-
-
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			ptr_dest[i] = ptr_src[i];
+		}
+	}
 	return (dest);
-
-
 }
-
-// 	 The memory areas may overlap: copying takes place as though the bytes in src are first copied into
-//        a temporary array that does not overlap src or dest, and the bytes are then copied from the temporary array to dest.
-// }
